2f78dc07732a5e998bd886175e653224f8d26b97
petr.petrik.024 <petr.petrik.024@pslib.cz>
2026-06-07 23:17:22 +0200

    Random Studio 1.1.2

--- styles/toCart.js
+++ styles/toCart.js
@@ -13,3 +13,91 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     })
 });
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".cart2");
+    for (const btn of document.querySelectorAll(".cart2button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".cart3");
+    for (const btn of document.querySelectorAll(".cart3button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".cart4");
+    for (const btn of document.querySelectorAll(".cart4button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".cart5");
+    for (const btn of document.querySelectorAll(".cart5button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".cart6");
+    for (const btn of document.querySelectorAll(".cart6button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".cart7");
+    for (const btn of document.querySelectorAll(".cart7button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".cart8");
+    for (const btn of document.querySelectorAll(".cart8button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".cart9");
+    for (const btn of document.querySelectorAll(".cart9button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
