e513d173b6bb348ee0c0c77e236e547c87f80433
petr.petrik.024 <petr.petrik.024@pslib.cz>
2026-06-10 17:51:44 +0200

    Random Studio 1.1.3

--- styles/dots.js
+++ styles/dots.js
@@ -9,80 +9,80 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
-// document.addEventListener("DOMContentLoaded", () => {
-    // let hamelmnts = document.querySelectorAll(".dot2");
-    // for (const btn of document.querySelectorAll(".dot2button")) {
-        // btn.addEventListener("click", (e) => {
-            // e.preventDefault();
-            // for (const element of hamelmnts) {
-                // element.classList.toggle("active");
-            // }  
-        // });
-    // }
-// });
-// document.addEventListener("DOMContentLoaded", () => {
-    // let hamelmnts = document.querySelectorAll(".dot3");
-    // for (const btn of document.querySelectorAll(".dot3button")) {
-        // btn.addEventListener("click", (e) => {
-            // e.preventDefault();
-            // for (const element of hamelmnts) {
-                // element.classList.toggle("active");
-            // }  
-        // });
-    // }
-// });
-// document.addEventListener("DOMContentLoaded", () => {
-    // let hamelmnts = document.querySelectorAll(".dot4");
-    // for (const btn of document.querySelectorAll(".dot4button")) {
-        // btn.addEventListener("click", (e) => {
-            // e.preventDefault();
-            // for (const element of hamelmnts) {
-                // element.classList.toggle("active");
-            // }  
-        // });
-    // }
-// });
-// document.addEventListener("DOMContentLoaded", () => {
-    // let hamelmnts = document.querySelectorAll(".dot5");
-    // for (const btn of document.querySelectorAll(".dot5button")) {
-        // btn.addEventListener("click", (e) => {
-            // e.preventDefault();
-            // for (const element of hamelmnts) {
-                // element.classList.toggle("active");
-            // }  
-        // });
-    // }
-// });
-// document.addEventListener("DOMContentLoaded", () => {
-    // let hamelmnts = document.querySelectorAll(".dot6");
-    // for (const btn of document.querySelectorAll(".dot6button")) {
-        // btn.addEventListener("click", (e) => {
-            // e.preventDefault();
-            // for (const element of hamelmnts) {
-                // element.classList.toggle("active");
-            // }  
-        // });
-    // }
-// });
-// document.addEventListener("DOMContentLoaded", () => {
-    // let hamelmnts = document.querySelectorAll(".dot7");
-    // for (const btn of document.querySelectorAll(".dot7button")) {
-        // btn.addEventListener("click", (e) => {
-            // e.preventDefault();
-            // for (const element of hamelmnts) {
-                // element.classList.toggle("active");
-            // }  
-        // });
-    // }
-// });
-// document.addEventListener("DOMContentLoaded", () => {
-    // let hamelmnts = document.querySelectorAll(".dot8");
-    // for (const btn of document.querySelectorAll(".dot8button")) {
-        // btn.addEventListener("click", (e) => {
-            // e.preventDefault();
-            // for (const element of hamelmnts) {
-                // element.classList.toggle("active");
-            // }  
-        // });
-    // }
-// });
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".dot2");
+    for (const btn of document.querySelectorAll(".dot2button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".dot3");
+    for (const btn of document.querySelectorAll(".dot3button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".dot4");
+    for (const btn of document.querySelectorAll(".dot4button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".dot5");
+    for (const btn of document.querySelectorAll(".dot5button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".dot6");
+    for (const btn of document.querySelectorAll(".dot6button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".dot7");
+    for (const btn of document.querySelectorAll(".dot7button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let hamelmnts = document.querySelectorAll(".dot8");
+    for (const btn of document.querySelectorAll(".dot8button")) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+        });
+    }
+});
